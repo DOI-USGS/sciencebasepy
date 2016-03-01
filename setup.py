35b8eca569b1776192af6ebc5029370b49fff4c4
John L Long <jllong@usgs.gov>
2016-03-01 11:49:20 -0700

    v1.2.6

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='pysb',
-      version='1.2.5',
+      version='1.2.6',
       description="Python ScienceBase Utilities",
       author="ScienceBase Development Team",
       author_email="sciencebase@usgs.gov",

f3d1dd994e2078328d2d0206d5d340b58acefb80
Robb Prescott <rprescott@usgs.gov>
2016-03-02 09:32:35 -0700

    revert pep8 changes for review - Revert "move pysb into a package so it can be added more easily"
    This reverts commit c68a50978e44a93fb8d1217b3ad686453b5ce008.

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
 from distutils.core import setup
 setup(name='pysb',
-      version='1.3.0',
-      packages=['pysb',],
+      version='1.2.6',
       description="Python ScienceBase Utilities",
       author="ScienceBase Development Team",
       author_email="sciencebase@usgs.gov",
       url='https://my.usgs.gov/confluence/display/sciencebase/ScienceBase+Item+Services',
       py_modules=['pysb'],
-      install_requires=[
-        'requests'
-      ]
       )

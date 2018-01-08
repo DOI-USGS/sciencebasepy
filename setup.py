92a19b018f6490aacb0e83abf2bbd4849a591ebb
Steve Tekell <tekells@usgs.gov>
2018-01-08 14:29:39 -0700

    version update for login POST changes

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='pysb',
-      version='1.5.9',
+      version='1.5.10',
       packages=['pysb',],
       description="Python ScienceBase Utilities",
       author="ScienceBase Development Team",

e1c2e4c4d2efb0de22aa8514492a680707a46336
kanraj <27645951+kanraj@users.noreply.github.com>
2021-11-23 14:47:09 -0500

    fix test demoEx2

--- demoEx2.py
+++ demoEx2.py
@@ -1,19 +1,21 @@
 import sciencebasepy
+import time
 
 FILE_PATH = 'C:/Apogee Engineering/testupload.txt'
 FILE_NAME = 'testupload.txt'
-ITEM_ID = '61534834d34e0df5fb9c5c91'
+ITEM_ID = '619c1c66d34eb622f692fe99'
 
 sb = sciencebasepy.SbSession()
 
 
 # Get a private item.  Need to log in first.
 print("going to log in")
-username = "k.rajakr98@gmail.com"
-password = "usgsTest123!"
-sb.login(username, password)
+username = "kraja@contractor.usgs.gov"
+sb.loginc(username)
 print("logged in")
 print("My Items ID:" + str(sb.get_my_items_id()))
 
+time.sleep(5)
+
 # Upload a File using GraphQL
-#print(sb.upload_large_file(ITEM_ID, FILE_PATH, FILE_NAME))
+print(sb.upload_large_file(ITEM_ID, FILE_PATH, FILE_NAME))
